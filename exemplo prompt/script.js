f4183868715d04d0cf2955cb170487ae354fcd10
IsabellePacheco <isapachecosenai@gmail.com>
2025-02-03 11:44:20 -0300

    Exemplo Prompt

---
+++ exemplo prompt/script.js
@@ -0,0 +1,11 @@
+let nome = prompt(" Qual seu nome?");
+console.log (nome)
+alert(nome)
+
+document.getElementById("resultado").innerText ="OLÁ, " + nome;
+
+let anos = prompt("Quantos nos você tem?");
+console.log (anos)
+alert(anos)
+
+document.getElementById("anos").innerText ="Você tem " + anos + " anos.";

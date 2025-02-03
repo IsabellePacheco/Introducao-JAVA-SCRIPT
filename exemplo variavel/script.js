ff7550bb4924638f8a85f91e99582e91476080ee
IsabellePacheco <isapachecosenai@gmail.com>
2025-02-03 11:44:31 -0300

    Exemplo Variavel

---
+++ exemplo variavel/script.js
@@ -0,0 +1,17 @@
+// variavel let- level, simpes (serve para um bloco especifico)
+// const
+//var
+let nome = ("Isa")
+let endereco =("Das luzes")
+//console.log(nome)
+//alert(nome)
+
+
+// para buscar irformações com o ID do html resultados
+// tipo texto 
+// teg p
+
+
+// o + serve para juntar dados 
+document.getElementById("resultado").innerText = "Olá, seu nome é: " + nome + ", você mora no bairro: " 
++ endereco + ", Boa tarde"

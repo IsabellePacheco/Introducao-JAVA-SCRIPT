ee5f511963debddd35dc8551553bf39b0e661cec
IsabellePacheco <isapachecosenai@gmail.com>
2025-02-04 10:29:44 -0300

    exemplo soma-botão

---
+++ exemplo soma-botao/script.js
@@ -0,0 +1,9 @@
+let num1 = parseInt(prompt("Digite um número"))
+let num2 = parseInt(prompt("Digite outro número"))
+let soma = num1 + num2
+
+
+const Clicou = () => {
+    // forma certa do alerte é (window.alert)
+    window.alert(document.getElementById("resultado").innerText = soma) 
+}

083bb43029b0b6c5df14816395af2adf77067770
IsabellePacheco <isapachecosenai@gmail.com>
2025-02-04 10:29:52 -0300

    exemplo soma

---
+++ exemplo soma/script.js
@@ -0,0 +1,11 @@
+let num1 = parseInt(prompt("Digite um número"))
+// prompt mostra 
+let num2 = parseInt(prompt("Digite outro número"))
+let soma = (num1) + (num2)
+// "number" serve para somar uma variavel
+// let soma = Number(num1) + Number(num2)
+document.getElementById("resultado").innerText = "A soma é igual a  " + soma
+
+
+// parseInt- numero inteiro
+// parseFloat- numero com virgula
